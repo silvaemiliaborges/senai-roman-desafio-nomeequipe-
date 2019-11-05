0f2988773b2d21b06704515f829240819d37ff7b
Emilia Da Silva Borges <43121017870@info.local>
2019-11-05 17:26:24 -0300

    up

--- Sql/SQLQuery1.sql
+++ Sql/SQLQuery1.sql
@@ -13,6 +13,8 @@ create table Professor (
     IdProfessor int primary key identity
 	,NomeDoProfessor varchar (255) not null
 	,IdTipoUsuario int foreign key references TipoUsuario(IdTipoUsuario)
+	,Email varchar (255) not null unique
+	,Senha varchar (255) not null 
 );
 create table Projeto (
 	
@@ -27,11 +29,11 @@ create table Projeto (
 insert into TipoUsuario(Nome)
 values ('Professor'), ('Aluno'), ('Outros');
 
-insert into Professor (NomeDoProfessor, IdTipoUsuario)
+insert into Professor (NomeDoProfessor, IdTipoUsuario, Email, Senha)
 
-values ('Edinalva', 1),
-		('Robson', 2),
-		('Barbara',3);
+values ('Edinalva', 1, 'Edi@nalva.com', '123456'),
+		('Robson', 2, 'rob@son.com', '654321'),
+		('Barbara',3, 'bar@bara.com', '987456');
 
 insert into Projeto (Nome, Tema, NomeDoProfessor)
 values ('Controle de Estoque', 'Gestão', 1 ),
@@ -40,3 +42,5 @@ values ('Controle de Estoque', 'Gestão', 1 ),
 select * from Projeto
 select * from Professor
 select * from TipoUsuario
+
+
